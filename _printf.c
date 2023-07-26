595287defa8b931c7472debbd5f5ff422f69204d
ashour <mohga.m.ashour@gmail.com>
2023-07-25 19:48:39 -0700

    pritf function

--- _printf.c
+++ _printf.c
@@ -1,33 +1,48 @@
 #include "main.h"
+/**
+ * _printf - formatted output conversion and print data.
+ * @format: input string.
+ *
+ * Return: number of chars printed.
+ */
 int _printf(const char *format, ...)
 {
-	int i, len;
+	int sum = 0; /* we will return that at the end*/
 	va_list args;
-	char *x;
-	char *buffer;
+	char *p, *start; /*pointers*/
+	params_t params = PARAMS_INIT; /*flags*/
 
 	va_start(args, format);
-	len = _strlen(format);
-	buffer = malloc(sizeof(char) * BUFF_SIZE)
 
-	for(i = 0; i < len; i++)
+	if(!format || (format[0] == '%' && !format[i]))/*check for the format that is not null*/
+		return (-1);
+	if(format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
+	for (p = (char *)format; *p; p++)/* loop on the format characters an *p in not null*/
 	{
-		if (format[i] == '%')
+		init_params(&params, args);
+		if (*p != '%')
 		{
-			i++;
-			if (format[i] == 'c')
-			{
-				x = va_arg(args, char);
-				print_buf(buffer, x);
-			}
-			if (format[i] == 's')
-			{
-				x = va_arg(args, char*);
-				print_buf(buffer, x);
-			}
+			sum += _putchar(*p);
+			continue;
 		}
-		free(buffer);
+		start = p;
+		p++;
+		while (get_flag(p, &params)) /*while char at p is flag char*/
+		{
+			p++; /*next char*/
+		}
+		p = get_width(p, &params, args);
+		p = get_presentage(p, &params, args);
+		if (get_modifire(p, &params)
+			p++;
+		if (!get_specifire(p))
+			sum += print_from_to(start, p,
+				params.1_modifire || params.h_modifire ? p - 1 : 0);
+		else
+			sum += get_print_fun(p, args, &params);
 	}
-	va_end(args);
-	return (len);
+	_putchar(BUF_FLUSH);
+	va_end(args)
+	return (sum);
 }
